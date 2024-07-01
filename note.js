af9fb601738e6d433a21c3e048b79ddf16c3a143
maissoumaboudrare <me@maissoum.dev>
2024-07-01 22:41:33 +0200

    Next step HttpOnly

---
+++ note.js
@@ -0,0 +1,15 @@
+
+/*!
+Quand j'ajoute le cookie dans postman cela fonctionne : { isAuthenticated: true }
+Quand je me co en front, il n'y a pas de cookie de créé (postman) { isAuthenticated: false } et token = undefined
+>> mais en front oui
+
+*/
+
+// Anime du Like
+
+
+/* [ TODO ]
+! suppressHydrationWarning on Layout.tsx
+
+*/
